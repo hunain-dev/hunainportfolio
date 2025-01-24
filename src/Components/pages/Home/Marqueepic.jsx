a5b572bd08b8daa3d203875f1bfcddc94d8433ec
hunain soomro <huniiali1122@gmail.com>
2025-01-24 23:20:11 +0500

    Your commit message here

--- src/Components/pages/Home/Marqueepic.jsx
+++ src/Components/pages/Home/Marqueepic.jsx
@@ -1,12 +1,11 @@
 const Marqueepic = () => {
-    return (
-      <>
-        <div className="imageupper">
-          <img src="/public/Assets/images/nain.png" alt="Profile" />
-        </div>
-      </>
-    );
-  };
-  
-  export default Marqueepic;
-  
+  return (
+    <>
+      <div className="imageupper">
+        <img src="/Assets/images/nain.png" alt="nain.png" />
+      </div>
+    </>
+  );
+};
+
+export default Marqueepic;
--- src/Components/pages/Project/Certified.jsx
+++ src/Components/pages/Project/Certified.jsx
@@ -1,25 +1,26 @@
 const Certified = () => {
     const data = [
-        {
-            cardimg:'/public/Assets/images/winning.jpg',
-        },
+      {
+        cardimg: '/Assets/images/winning.jpg', // Corrected image path
+      },
     ];
-
+  
     return (
-        <div className="CertifiedCard">
-            <h2>1st Runner-Up in Web Design | Techwiz  Pakistan Web Design Competition</h2> {/* ✅ Upar fix */}
-
-            <div className="Certifiedcards">
-                {data.map((elem, index) => (
-                    <div key={index}>
-                        <div className="Certifiedcardsimage">
-                            <img src={elem.cardimg} alt="Certification" />
-                        </div>
-                    </div>
-                ))}
+      <div className="CertifiedCard">
+        <h2>1st Runner-Up in Web Design | Techwiz Pakistan Web Design Competition</h2> {/* ✅ Upar fix */}
+  
+        <div className="Certifiedcards">
+          {data.map((elem, index) => (
+            <div key={index}>
+              <div className="Certifiedcardsimage">
+                <img src={elem.cardimg} alt="Certification" />
+              </div>
             </div>
+          ))}
         </div>
+      </div>
     );
-};
-
-export default Certified;
+  };
+  
+  export default Certified;
+  
