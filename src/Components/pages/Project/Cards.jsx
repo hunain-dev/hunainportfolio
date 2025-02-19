727b145a87c258af6b01526558f5efc51c1ddef7
hunain soomro <huniiali1122@gmail.com>
2025-02-19 21:02:55 +0500

    First commit

--- src/Components/pages/Project/Cards.jsx
+++ src/Components/pages/Project/Cards.jsx
@@ -5,15 +5,20 @@ const Cards = () => {
 
     const data = [
         {
-            cardh4: 'Ochi Award-Winning Web Designer | Specialized in Animated Websites with ReactJS, GSAP, and Framer Motion',
-            cardimg: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/0daa4d145487469.62d94b40df0af.png',
-            link: 'https://ochi-design-production.netlify.app/'
+            cardimg: '/Assets/images/spacer.png',  // Corrected the path here
+            cardh4: 'spencergabor.work Digital agency website | (Coming soon)',
         },
+     
       
         {
             cardimg: '/Assets/images/Web 1920 – 1.png',
             cardh4: 'Creator of Two Good Company Website',
             link: 'https://twogoodcompany.netlify.app/'
+        },
+        {
+            cardh4: 'Ochi Award-Winning Web Designer | Specialized in Animated Websites with ReactJS, GSAP, and Framer Motion',
+            cardimg: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/0daa4d145487469.62d94b40df0af.png',
+            link: 'https://ochi-design-production.netlify.app/'
         },
           {
             cardimg: '/Assets/images/nature.jpg',
@@ -33,10 +38,7 @@ const Cards = () => {
             videoSrc: '/Assets/images/croing.mp4', // Path to the video
         },
       
-        {
-            cardimg: '/Assets/images/spacer.png',  // Corrected the path here
-            cardh4: 'spencergabor.work Digital agency website | (Coming soon)',
-        },
+        
         
       
     ];
